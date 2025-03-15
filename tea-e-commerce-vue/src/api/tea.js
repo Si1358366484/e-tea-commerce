f4aef3a53d090f8f5cea6a2b3f2e6933768fa76a
Si1358366484 <15577672+si1358366484@user.noreply.gitee.com>
2025-03-15 19:50:36 +0800

    编写后端茶叶信息管理的添加和修改

--- tea-e-commerce-vue/src/api/tea.js
+++ tea-e-commerce-vue/src/api/tea.js
@@ -11,4 +11,12 @@ export const teaDeleteServise = (id)=>{
     return request.delete(`/admin/tea/${id}`)
 }
 
+//提供调用添加茶叶接口的函数    
+export const teaAddService = (teaData)=>{
+    return request.post('/admin/tea',teaData)
+}
 
+//提供调用编辑茶叶接口的函数
+export const teaEditService = (teaData)=>{
+    return request.put(`/admin/tea/${teaData.id}`,teaData)
+}
