bb8db65f33f9a669e7f3a08047d510a76442b484
Si1358366484 <15577672+si1358366484@user.noreply.gitee.com>
2025-03-17 23:29:15 +0800

    编写订单管理页面

--- tea-e-commerce-vue/src/views/order.vue
+++ tea-e-commerce-vue/src/views/order.vue
@@ -1,5 +1,214 @@
+<script setup>
+import {
+    Edit,
+    Delete
+} from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const categorys = ref([
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 5,
+        "categoryName": "军事",
+        "categoryAlias": "js",
+        "createTime": "2023-09-02 12:08:33",
+        "updateTime": "2023-09-02 12:08:33"
+    }
+    
+])
+</script>
 <template>
-  <div>
-      <h1>订单信息管理</h1>
-  </div>
+    <el-card class="page-container">
+        <template #header>
+            <div class="header">
+                <span>订单管理</span>
+            </div>
+        </template>
+        <!-- 搜索表单 -->
+        <el-form inline>
+            <el-form-item label="订单编号:">
+              <el-input v-model="categoryName" placeholder="请输入订单编号" style="width: 200px;"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="ElMessage.success('更新成功')">搜索</el-button>
+            </el-form-item>
+        </el-form>
+        <el-table :data="categorys" class="table" height="482">
+            <el-table-column label="序号" width="100" type="index"> </el-table-column>
+            <el-table-column label="订单编号" prop="categoryName"></el-table-column>
+            <el-table-column label="订单总价" prop="categoryName"></el-table-column>
+            <el-table-column label="商品名称" prop="categoryName"></el-table-column>
+            <el-table-column label="商品数量" prop="categoryName"></el-table-column>
+            <el-table-column label="商品状态" prop="categoryAlias"></el-table-column>
+            <el-table-column label="收货人" prop="categoryAlias"></el-table-column>
+            <el-table-column label="操作">
+                <template #default="{ }">
+                  <el-button type="success">发货</el-button>
+                  <el-button type="danger">退货</el-button>
+                </template>
+            </el-table-column>
+            <template #empty>
+                <el-empty description="没有数据" />
+            </template>
+        </el-table>
+    </el-card>
 </template>
+
+<style lang="scss" scoped>
+.page-container {
+    min-height: 100%;
+    box-sizing: border-box;
+
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        span {
+            font-size: 20px;
+            font-weight: bold;
+            color: #333;
+            }
+    }
+    .table {
+        width: 100%;
+    }
+}
+</style>
