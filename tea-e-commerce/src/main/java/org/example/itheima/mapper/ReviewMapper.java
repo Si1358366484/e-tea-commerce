7119ebb98c1c51d517b5b78dc9ec9d2b7b71099f
Si1358366484 <15577672+si1358366484@user.noreply.gitee.com>
2025-03-28 21:46:35 +0800

    修改评论顺序和删除功能

--- tea-e-commerce/src/main/java/org/example/itheima/mapper/ReviewMapper.java
+++ tea-e-commerce/src/main/java/org/example/itheima/mapper/ReviewMapper.java
@@ -10,7 +10,7 @@ public interface ReviewMapper {
     @Insert("insert into review(customer_name,tea_name,grade,content,review_time,state) " +
             "values(#{customerName},#{teaName},#{grade},#{content},#{reviewTime},#{state})")
     void createReview(Review review);
-    @Select("SELECT * FROM review LIMIT #{offset}, #{pageSize}")
+    @Select("SELECT * FROM review ORDER BY review_time DESC, id DESC LIMIT #{offset}, #{pageSize}")
     List<Review> getReviewsByPage(@Param("offset") int offset, @Param("pageSize") int pageSize);
     @Select("SELECT COUNT(*) FROM review")
     int getTotalReviews();
