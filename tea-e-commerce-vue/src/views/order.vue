71d9785ace5db448b4b470c27d4f651c9a5f0135
Si1358366484 <15577672+si1358366484@user.noreply.gitee.com>
2025-03-29 21:28:17 +0800

    修改后台订单管理结构

--- tea-e-commerce-vue/src/views/order.vue
+++ tea-e-commerce-vue/src/views/order.vue
@@ -6,6 +6,7 @@ import {
 import { ref } from 'vue'
 import { ElMessage } from 'element-plus'
 import { getOrderListService,updateStateService,searchOrderService } from '@/api/order'
+import { Goods, RefreshLeft, Van } from '@element-plus/icons-vue'
 const orders = ref()
 const orderReference = ref()
 const getOrderList = async () => {
@@ -17,7 +18,8 @@ getOrderList()
 const updateState = async (state,id) => {
     let result = await updateStateService(state,id)
     ElMessage.success('更新成功')
-    getOrderList()
+    getOrderList() 
+    orderCount.value = filterOrders().length
 }
 const search = async (orderReference) => {
     let result = await searchOrderService(orderReference)
@@ -38,16 +40,70 @@ const rowStyle = ({ row }) => {
     }
     return '';
 };
+const orderCount = ref(0); // 待处理数量
+//分类数据模型
+const operation = ref([
+    {
+        "id": 1,
+        "operationName": "发货",
+    },
+    {
+        "id": 2,
+        "operationName": "退货",
+    }
+])
+const operationName = ref('')
+// 添加过滤方法
+const filterOrders = () => {
+  if (operationName.value === '发货') {
+    const filtered = orders.value.filter(item => item.state === '已付款')
+    orderCount.value = filtered.length
+    return filtered
+  } else if (operationName.value === '退货') {
+    const filtered = orders.value.filter(item => item.state === '已发货')
+    orderCount.value = filtered.length
+    return filtered
+  }
+  return orders.value
+}
 </script>
 <template>
     <el-card class="page-container">
         <template #header>
             <div class="header">
                 <span>订单管理</span>
+                <!-- 消息通知栏 -->
+                <el-button v-if="operationName=='退货'" class="order-btn">
+                    <el-badge :value="orderCount" :max="99">
+                        <el-icon :size="20">
+                            <RefreshLeft />
+                        </el-icon>
+                    </el-badge>
+                </el-button>
+                <el-button v-else-if="operationName=='发货'" class="order-btn">
+                    <el-badge :value="orderCount" :max="99">
+                        <el-icon :size="20">
+                            <Van />
+                        </el-icon>
+                    </el-badge>
+                </el-button>
             </div>
         </template>
         <!-- 搜索表单 -->
         <el-form inline>
+            <el-form-item label="操作：">
+                <el-select placeholder="请选择" v-model="operationName" style="width: 200px;">
+                    <el-option 
+                        v-for="c in operation" 
+                        :key="c.id" 
+                        :label="c.operationName"
+                        :value="c.operationName">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item>
+                <el-button @click="operationName=''">重置</el-button>
+            </el-form-item>
             <el-form-item label="订单编号:">
               <el-input v-model="orderReference" placeholder="请输入订单编号" style="width: 200px;"></el-input>
             </el-form-item>
@@ -55,7 +111,7 @@ const rowStyle = ({ row }) => {
                 <el-button type="primary" @click="search(orderReference)">搜索</el-button>
             </el-form-item>
         </el-form>
-        <el-table :data="orders" class="table" height="482"  :row-class-name="rowClassName">
+        <el-table :data="filterOrders()" class="table" height="482"  :row-class-name="rowClassName">
             <el-table-column label="序号" width="100" type="index"> </el-table-column>
             <el-table-column label="订单编号" prop="orderReference"></el-table-column>
             <el-table-column label="订单总价" prop="orderAmounts"></el-table-column>
@@ -68,11 +124,11 @@ const rowStyle = ({ row }) => {
                     <span v-else-if="row.state === '已退货'" style="color: red;">已退货</span>
                 </template>
             </el-table-column>
-            <el-table-column label="操作">
+            <el-table-column label="操作" v-if="operationName != ''">
                 <template #default="{ row }">
                   <!-- 添加 :disabled="row.state === '已退货'" 禁用按钮 -->
-                  <el-button type="success" @click="updateState('已发货',row.orderReference)" :disabled="row.state === '已退货'">发货</el-button>
-                  <el-button type="danger" @click="updateState('已退货',row.orderReference)" :disabled="row.state === '已退货'">退货</el-button>
+                  <el-button v-if="operationName=='退货'" type="danger" @click="updateState('已退货',row.orderReference)" :disabled="row.state === '已退货'">退货</el-button>
+                  <el-button v-else type="success" @click="updateState('已发货',row.orderReference)" :disabled="row.state === '已退货'">发货</el-button>
                 </template>
             </el-table-column>
             <template #empty>
@@ -83,6 +139,14 @@ const rowStyle = ({ row }) => {
 </template>
 
 <style lang="scss" scoped>
+.order-btn {
+  padding: 8px;
+  border: none;
+}
+
+.order-btn:hover {
+  background-color: var(--el-color-primary-light-9);
+}
 :deep(.canceled-row) {
     position: relative; /* 确保伪元素定位正确 */
 }
